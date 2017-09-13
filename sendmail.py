45fc6fb0b1dd196cf69aa9673687daa45a2f4044
Aston-95 <marvdb95@gmail.com>
2017-09-14 00:00:12 +0200

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -3,21 +3,12 @@ import smtplib;
 class envoimail:
 
     def __init__(self, mail):
-
         mail = smtplib.SMTP('smtp.gmail.com',587);
-
         mail.ehlo();
-
         mail.starttls();
 
-    def sendmail(self, content):
-
-        content = ("Il y'a un problème avec la barrière !");
-
+    def sendmail(self):
         mail.login('lepreux95520@gmail.com','motdepasse123');
-
-        mail.sendmail('lepreux95520@gmail.com','lepreux95520@gmail.com',content); 
-
+        mail.sendmail('lepreux95520@gmail.com','lepreux95520@gmail.com','Il y\'a un problème avec la barrière'); 
         mail.close();
-
         print("Sent");
